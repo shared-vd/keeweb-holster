1b00cc477cdbf737397ae69de57547b28ff39731
Jean-Eric Cuendet <jec@jesc.h>
2018-02-23 18:13:20 +0100

    Initial

---
+++ src/main/java/ch/vd/keeweb/KeeWebApplication.java
@@ -0,0 +1,12 @@
+package ch.vd.keeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeeWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeeWebApplication.class, args);
+    }
+}
---
+++ src/test/java/ch/vd/keeweb/KeeWebTomcatRunner.java
@@ -0,0 +1,8 @@
+package ch.vd.keeweb;
+
+public class KeeWebTomcatRunner {
+
+    public static void main(String ... args) {
+        KeeWebApplication.main(args);
+    }
+}
